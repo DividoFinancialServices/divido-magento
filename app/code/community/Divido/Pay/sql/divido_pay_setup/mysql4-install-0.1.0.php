5c92b1b3dc1b7516fa460a887beff1c948187d6f
Jonas Karlsson <jonas.karlsson@stim.se>
2015-12-08 17:08:52 +0100

    cleaned up

--- app/code/community/Divido/Pay/sql/divido_pay_setup/mysql4-install-0.1.0.php
+++ app/code/community/Divido/Pay/sql/divido_pay_setup/mysql4-install-0.1.0.php
@@ -13,27 +13,28 @@ $setup->addAttributeGroup('catalog_product', 'Default', 'Divido', 1000);
 // the attribute added will be displayed under the group/tab Special Attributes in product edit page
 						
 $setup->addAttribute('catalog_product', 'divido', array(
-			 'label'             => 'Finance by Divido',
-			 'type'              => 'varchar',
-			 'input'             => 'select',
-			 'backend'           => 'eav/entity_attribute_backend_array',
-			 'frontend'          => '',	
-			 'source'            => 'pay/source_option',
-			 'global'            => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_GLOBAL,
-			 'visible'           => true,
-			 'required'          => true,
-			 'user_defined'      => true,
-			 'searchable'        => true,
-			 'filterable'        => false,
-			 'comparable'        => false,
-			 'option'            => array ('value' => array('optionone' => array('Yes'),
-									 'optiontwo' => array('No')
- 												
-								)
-							),
-			 'visible_on_front'  => true,
-			 'visible_in_advanced_search' => true,
-			 'unique'            => false
+    'label'        => 'Finance by Divido',
+    'type'         => 'varchar',
+    'input'        => 'select',
+    'backend'      => 'eav/entity_attribute_backend_array',
+    'frontend'     => '',
+    'source'       => 'pay/source_option',
+    'global'       => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_GLOBAL,
+    'visible'      => true,
+    'required'     => true,
+    'user_defined' => true,
+    'searchable'   => true,
+    'filterable'   => false,
+    'comparable'   => false,
+    'option'            => array (
+        'value' => array(
+            'optionone' => array('Yes'),
+            'optiontwo' => array('No'),
+        )
+    ),
+    'visible_on_front'           => true,
+    'visible_in_advanced_search' => true,
+    'unique'                     => false
 ));
  
 $installer->endSetup();
