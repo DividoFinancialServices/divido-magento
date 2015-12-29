0e594e8077dee82516bee60bfaab6e21ab905a26
Jonas Karlsson <jonas.karlsson@stim.se>
2015-12-29 10:24:36 +0100

    display price calc on prod page

--- src/app/code/community/Divido/Pay/Block/Price.php
+++ src/app/code/community/Divido/Pay/Block/Price.php
@@ -3,7 +3,7 @@ class Divido_Pay_Block_Price extends Mage_Core_Block_Template
 {
     public function getPrice ()
     {
-        $price = 1000;
+        $price = 89898;
 
         return $price;
     }
