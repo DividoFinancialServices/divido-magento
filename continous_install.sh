4e78d71591e38a4d0432e40c18cc35d7ecfb6f5a
Jonas Karlsson <jonas.karlsson@stim.se>
2015-12-30 11:57:29 +0100

    Don't track development files

--- continous_install.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-fswatch -o src/ | xargs -n1 -I{} ./install.sh
--- install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cp -R ~/Dropbox/Work/Divido/src/magento-module/src/* ~/Dropbox/Work/Divido/src/magento1/
-date
