fb86f9e8c8deedf5a4f5652284879d9ed6f32075
Jonas Karlsson <jonas.karlsson@stim.se>
2015-12-30 11:58:21 +0100

    Relative callback URLs

--- src/app/code/community/Divido/Pay/Controller/Payment/Abstract.php
+++ src/app/code/community/Divido/Pay/Controller/Payment/Abstract.php
@@ -1,30 +1,4 @@
 <?php
-
-/**
- * Magento
- *
- * NOTICE OF LICENSE
- *
- * This source file is subject to the Open Software License (OSL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
- * It is also available through the world-wide-web at this URL:
- * http://opensource.org/licenses/osl-3.0.php
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to license@magentocommerce.com so we can send you a copy immediately.
- *
- * DISCLAIMER
- *
- * Do not edit or add to this file if you wish to upgrade Magento to newer
- * versions in the future. If you wish to customize Magento for your
- * needs please refer to http://www.magentocommerce.com for more information.
- *
- * @category    Mage
- * @package     Divido_Pay
- * @copyright   Copyright (c) 2014 Magento Inc. (http://www.magentocommerce.com)
- * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
- */
-
 /**
  * Abstract Standard Checkout Controller
  */
@@ -150,13 +124,14 @@ abstract class Divido_Pay_Controller_Payment_Abstract extends Mage_Core_Controll
                 'phone_number'  => $telephone,
             ),
             'products' => $products,
-            'response_url' => 'http://cjgk.me/response.php',
+            'response_url' => Mage::getBaseUrl(Mage_Core_Model_Store::URL_TYPE_WEB) . 'divido_callback.php',
             'redirect_url' => Mage::getUrl('customer/account/'),
         );
+        xdebug_break();
         $response = Divido_CreditRequest::create($request_data);
 
         if ($response->status == 'ok') {
-            $this->_redirectUrl($response->url);
+            //$this->_redirectUrl($response->url);
         } else {
             if ($response->status === 'error') {
                 Mage::getSingleton('checkout/session')->addError($response->error);
