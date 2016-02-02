73ec5f2f6e7bbbea31baa779a0536b0718b5d109
Jonas Hässel <i@cjkg.me>
2016-02-02 10:48:39 +0100

    build releases with version number

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
-if [ ! -d "$DIRECTORY" ]; then
-    mkdir build
+if [ ! -d release ]; then
+    mkdir release
 fi
 
+version=$(xmllint --xpath '/config/modules/Divido_Pay/version/text()' src/app/code/community/Divido/Pay/etc/config.xml)
+
 cd src/
-zip  -r ../release/divido-magento.zip *
+zip  -r ../release/divido-magento-$version.zip *
 cd ../
