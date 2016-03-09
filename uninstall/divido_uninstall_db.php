d7201bf4257b307eb907670ab28dc13e4f1709dd
Jonas Hässel <i@cjkg.me>
2016-03-09 15:16:19 +0100

    Remove all settings

--- uninstall/divido_uninstall_db.php
+++ uninstall/divido_uninstall_db.php
@@ -9,3 +9,4 @@ $resource = Mage::getSingleton('core/resource');
 $writeConn = $resource->getConnection('core_write');
 $writeConn->query('drop table if exists divido_lookup');
 $writeConn->query("delete from core_resource where code = 'divido_pay_setup'");
+$writeConn->query("delete from core_config_data where core_config_data.path like 'payment/pay/%'");
