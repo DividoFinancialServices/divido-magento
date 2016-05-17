9c719c42537f3de75929320a4f4d224580951ff8
Jonas Hässel <i@cjkg.me>
2016-05-17 11:43:28 +0200

    Respond with version. Handle statuses in a better way.

--- src/divido_callback.php
+++ src/divido_callback.php
@@ -4,28 +4,35 @@ umask(0);
 
 Mage::app('admin');
 
-define('STORE',               1);
-define('STATUS_ACCEPTED',     'ACCEPTED');
-define('STATUS_CANCELED',     'CANCELED');
-define('STATUS_COMPLETED',    'COMPLETED');
-define('STATUS_DEFERRED',     'DEFERRED');
-define('STATUS_DECLINED',     'DECLINED');
-define('STATUS_DEPOSIT_PAID', 'DEPOSIT-PAID');
-define('STATUS_FULFILLED',    'FULFILLED');
-define('STATUS_REFERRED',     'REFERRED');
-define('STATUS_SIGNED',       'SIGNED');
+define('STORE',                1);
+define('STATUS_ACCEPTED',      'ACCEPTED');
+define('STATUS_ACTION_LENDER', 'ACTION-LENDER');
+define('STATUS_CANCELED',      'CANCELED');
+define('STATUS_COMPLETED',     'COMPLETED');
+define('STATUS_DEFERRED',      'DEFERRED');
+define('STATUS_DECLINED',      'DECLINED');
+define('STATUS_DEPOSIT_PAID',  'DEPOSIT-PAID');
+define('STATUS_FULFILLED',     'FULFILLED');
+define('STATUS_REFERRED',      'REFERRED');
+define('STATUS_SIGNED',        'SIGNED');
 
 $historyMessages = array(
-    STATUS_ACCEPTED     => 'Credit request accepted',
-    STATUS_CANCELED     => 'Application canceled',
-    STATUS_COMPLETED    => 'Application completed',
-    STATUS_DEFERRED     => 'Application deferred by Underwriter, waiting for new status',
-    STATUS_DECLINED     => 'Applicaiton declined by Underwriter',
-    STATUS_DEPOSIT_PAID => 'Deposit paid by customer',
-    STATUS_FULFILLED    => 'Credit request fulfilled',
-    STATUS_REFERRED     => 'Credit request referred by Underwriter, waiting for new status',
-    STATUS_SIGNED       => 'Customer have signed all contracts',
+    STATUS_ACCEPTED      => 'Credit request accepted',
+    STATUS_ACTION_LENDER => 'Lender notified',
+    STATUS_CANCELED      => 'Application canceled',
+    STATUS_COMPLETED     => 'Application completed',
+    STATUS_DEFERRED      => 'Application deferred by Underwriter, waiting for new status',
+    STATUS_DECLINED      => 'Applicaiton declined by Underwriter',
+    STATUS_DEPOSIT_PAID  => 'Deposit paid by customer',
+    STATUS_FULFILLED     => 'Credit request fulfilled',
+    STATUS_REFERRED      => 'Credit request referred by Underwriter, waiting for new status',
+    STATUS_SIGNED        => 'Customer have signed all contracts',
 );
+
+function exitWithVersion() {
+    $version = Mage::getConfig()->getModuleConfig("Divido_Pay")->version;
+    exit("M1-{$version}");
+}
 
 $noGo = array(STATUS_CANCELED, STATUS_DECLINED);
 
@@ -36,14 +43,14 @@ $lookup = Mage::getModel('callback/lookup');
 $lookup->load($data->metadata->quote_id, 'quote_id');
 if (! $lookup->getId()) {
     Mage::log('Bad request, could not find lookup. Req: ' . serialize($data), null, 'divido.log');
-    exit('Cannot verify request');
+    exit('Can not verify request');
 }
 
 $salt = $lookup->getSalt();
 $hash = Mage::helper('pay')->hashQuote($salt, $data->metadata->quote_id);
 if ($hash !== $data->metadata->quote_hash) {
     Mage::log('Bad request, mismatch in hash. Req: ' . serialize($data), null, 'divido.log');
-    exit('Cannot verify request');
+    exit('Can not verify request');
 }
 
 $lookup->setCreditApplicationId($data->application);
@@ -52,8 +59,26 @@ $lookup->save();
 $order = Mage::getModel('sales/order')->loadByAttribute('quote_id', $data->metadata->quote_id);
 
 if (! $order->getId() && in_array($data->status, $noGo)) {
-    Mage::log("Quote: {$data->metadata->quote_id}, Status: {$data->status}", null, 'divido.log');
-    exit('ok');
+    if ($data->status == STATUS_DECLINED) {
+        $lookup->setDeclined(1);
+    } elseif ($data->status == STATUS_CANCELED) {
+        $lookup->setCanceled(1);
+    }
+
+    $lookup->save();
+    exitWithVersion();
+}
+
+if ($order->getId() && $data->status === STATUS_DECLINED) {
+    $history = $order->addStatusHistoryComment("Divido: {$historyMessages[$data->status]}.", false);
+
+    $order->cancel();
+    $order->save();
+
+    $lookup->setDeclined(1);
+    $lookup->save();
+
+    exitWithVersion();
 }
 
 if (! $order->getId()) {
@@ -61,7 +86,7 @@ if (! $order->getId()) {
         ->setStore($store)
         ->load($data->metadata->quote_id);
 
-    // convert quote to order
+    // Convert quote to order
     $quote->collectTotals();
     $quote_service = Mage::getModel('sales/service_quote', $quote);
     $quote_service->submitAll();
@@ -69,8 +94,10 @@ if (! $order->getId()) {
 
     $order = $quote_service->getOrder();
     $order->setData('state', 'new');
-    //$order->setStatus('pending_payment');
 }
+
+$lookup->setOrderId($order->getId());
+$lookup->save();
 
 /*
 if ($data->status === STATUS_DEPOSIT_PAID) {
@@ -79,7 +106,7 @@ if ($data->status === STATUS_DEPOSIT_PAID) {
 */
 
 if ($data->status === STATUS_SIGNED) {
-    $order->setData('state', 'processing');
+    $order->setData('status', 'processing');
     $order->queueNewOrderEmail();
 }
 
@@ -90,4 +117,4 @@ if (isset($historyMessages[$data->status])) {
 
 $order->save();
 
-echo "ok";
+exitWithVersion();
