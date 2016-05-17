636d0765dc55a9247e6ad891de5d4683133c6c84
Jonas Hässel <i@cjkg.me>
2016-05-17 11:39:56 +0200

    add new columns to database

--- src/app/code/community/Divido/Pay/sql/divido_pay_setup/mysql4-upgrade-0.7.1-0.7.2.php
+++ src/app/code/community/Divido/Pay/sql/divido_pay_setup/mysql4-upgrade-0.7.1-0.7.2.php
@@ -1,7 +1,7 @@
 <?php
 require_once('app/Mage.php');
 Mage::app()->setCurrentStore(Mage::getModel('core/store')->load(Mage_Core_Model_App::ADMIN_STORE_ID));
- 
+
 $installer = $this;
 
 $setup = new Mage_Eav_Model_Entity_Setup('core_setup');
@@ -10,17 +10,42 @@ $installer->startSetup();
 /**
  * Modify lookup table for Divido
  */
-$installer->getConnection()
-    ->addColumn($installer->getTable('callback/lookup'),
-        'deposit_amount', 
-        array(
-            'type'      => Varien_Db_Ddl_Table::TYPE_NUMERIC,
-            'nullable'  => false,
-            'precision' => 10,
-            'scale'     => 2,
-            'comment'   => 'Credit application ID',
-        )
-    );
+$installer->getConnection()->addColumn($installer->getTable('callback/lookup'),
+    'order_id',
+    array(
+        'type' => Varien_Db_Ddl_Table::TYPE_INTEGER,
+        'nullable' => true,
+        'comment' => 'Order ID',
+    )
+);
 
+$installer->getConnection() ->addColumn($installer->getTable('callback/lookup'),
+    'deposit_amount', 
+    array(
+        'type'      => Varien_Db_Ddl_Table::TYPE_NUMERIC,
+        'nullable'  => false,
+        'precision' => 10,
+        'scale'     => 2,
+        'comment'   => 'Credit application ID',
+    )
+);
+
+$installer->getConnection()->addColumn($installer->getTable('callback/lookup'),
+    'canceled',
+    array(
+        'type' => Varien_Db_Ddl_Table::TYPE_BOOLEAN,
+        'nullable' => true,
+        'comment' => 'The application has ben cancelled',
+    )
+);
+
+$installer->getConnection()->addColumn($installer->getTable('callback/lookup'),
+    'declined',
+    array(
+        'type' => Varien_Db_Ddl_Table::TYPE_BOOLEAN,
+        'nullable' => true,
+        'comment' => 'The application was denied'
+    )
+);
 
 $installer->endSetup();
