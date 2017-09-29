6eb955c3fcbfc7ad971d6fec6ef5efeaca16791b
Jonas Hässel <i@cjkg.me>
2017-09-29 16:34:21 +0200

    Method for fetching only active lookups

--- app/code/community/Divido/Pay/Model/Resource/Lookup.php
+++ app/code/community/Divido/Pay/Model/Resource/Lookup.php
@@ -6,4 +6,23 @@ class Divido_Pay_Model_Resource_Lookup extends Mage_Core_Model_Resource_Db_Abstr
     {
         $this->_init('callback/lookup', 'lookup_id');
     }
+
+    public function loadActiveByQuoteId (Divido_Pay_Model_Lookup $object, $quote_id)
+    {
+        $adapter = $this->_getReadAdapter();
+        $bind    = array('quote_id' => $quote_id);
+        $select  = $adapter->select()
+            ->from($this->getMainTable(), 'lookup_id')
+            ->where('quote_id = :quote_id')
+            ->where('invalidated_at is null');
+
+        $modelId = $adapter->fetchOne($select, $bind);
+        if ($modelId) {
+            $this->load($object, $modelId );
+        } else {
+            $object->setData(array());
+        }
+
+        return $this;
+    }
 }
